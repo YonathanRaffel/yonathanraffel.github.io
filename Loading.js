01fd4fa4c18b0f5197eb6f77f325388e4111f62a
Raffel <Sir.Raffel@gmail.com>
2017-03-02 20:42:23 +0700

    Improve Page Speed!

--- Loading.js
+++
@@ -1,8 +0,0 @@
-document.onreadystatechange = function () {
-  var state = document.readyState
-  if (state == 'complete') {
-    document.getElementById('interactive');
-    document.getElementById('idloading').style.visibility="hidden";
-    document.getElementById('idcontent').style.display = "block";
-  }
-}
